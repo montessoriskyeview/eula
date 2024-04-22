e908f54702113da9211d8d4e8cd96713eff61159
chubbyskink <gavinolsenai@gmail.com>
2024-04-22 11:44:37 -0700

    Revert "update"
    This reverts commit 9ce818acf6faefd08feebf573108ff90a8a0f423.

--- src/components/shared/Typography.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography as MuiTypography, TypographyProps } from "@mui/material";
-
-export const Typography = (props: TypographyProps) => {
-  return <MuiTypography {...props} sx={{ ...props.sx, fontFamily: "Medium", color: "white" }} />
-}
--- src/views/Contact.tsx
+++
@@ -1,9 +0,0 @@
-import { CanvasView } from "../components/shared/Canvas/CanvasView"
-
-export const Contact = () => {
-  return (
-    <CanvasView>
-      <></>
-    </CanvasView>
-  )
-}
